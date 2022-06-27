a085c359c4805bb59ba12b880611927a13909dd0
Daivison Morais <daivison13pinheiro@gmail.com>
2022-06-27 17:02:28 -0300

    renderização de mensagens pronta

--- script.js
+++ script.js
@@ -22,9 +22,19 @@ function salvarNome() {
 
 function alertaAcerto(acerto){
     console.log("nome cadastrado")
-    
+    const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/participants")
+    promise.then(participantes)
+    promise.catch(semParticipantes)
     conexaoSistema()   
-buscarMensagens()
+    buscarMensagens()
+}
+
+function participantes(resposta){
+    console.log("participantes carregados!")
+}
+function semParticipantes (resposta){
+    alert("Não foi possível carregar lista de participantes")
+    perguntaNome()
 }
 
 function alertarErro(error){
@@ -32,7 +42,6 @@ function alertarErro(error){
         alert("Nome já existente");
         perguntaNome()
     }
-perguntaNome()
 }
 
 
@@ -40,61 +49,66 @@ function buscarMensagens() {
     const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages")
     console.log(promise)
     promise.then(popularMensagens);
-    setTimeout(buscarMensagens, 3000)
+    
 }
+setInterval(buscarMensagens, 3000)
+
 
 function popularMensagens(resposta){
    listaDados = resposta.data
    renderizarMensagens();
-  
 }
 
 function renderizarMensagens() {
     const ulareaconversa = document.querySelector(".areaconversa");
     ulareaconversa.innerHTML = "";
-    let i;
-    for (i = 0; i < listaDados.length; i++) {
-        
+
+    for (let i = 0; i < listaDados.length; i++) {
+        //cor cinza
         if (listaDados[i].type === "status") {
             ulareaconversa.innerHTML += `
         <li class="corcinza caixaconversa">
-        <div class="tempo">${listaDados[i].time}</div>
         <div class="textoconversa">
+            <span class="tempo">(${listaDados[i].time})</span>
             <span class="nomeetodos"> ${listaDados[i].from}</span>
-            <span class="">${listaDados[i].text}</span>
+            <span>${listaDados[i].text}</span>
         </div>
         </li>
         `;
         }
 
-        if(listaDados[i].type === "message"){
+        //cor branca
+        if (listaDados[i].type === "message") {
             ulareaconversa.innerHTML += `
         <li class="caixaconversa">
-        <div class="tempo">${listaDados[i].time}</div>
         <div class="textoconversa">
+            <span class="tempo">(${listaDados[i].time})</span>
             <span class="nomeetodos"> ${listaDados[i].from}</span>
             <span class="margin">para</span>
-            <span class="nomeetodos"> ${listaDados[i].to}:</span>
-            <span class="">${listaDados[i].text}</span>
-        </div>
-        </li>
-        `;
-        }
-        if(listaDados[i].type === "private_message"){
-            ulareaconversa.innerHTML += `
-        <li class="corrosa caixaconversa">
-        <div class="tempo">(${listaDados[i].time})</div>
-        <div class="textoconversa">
-            <span class="nomeetodos"> ${listaDados[i].from}</span>
-            <span class="margin">reservadamente para</span>
             <span class="nomeetodos"> ${listaDados[i].to}:</span>
             <span class="quebra">${listaDados[i].text}</span>
         </div>
         </li>
         `;
         }
+
+        //cor rosa
+        if (listaDados[i].type === "private_message" && listaDados[i].to === nome) {
+                ulareaconversa.innerHTML += 
+                `
+                <li class="corrosa caixaconversa">
+                    <div class="textoconversa">
+                        <span class="tempo">(${listaDados[i].time})</span>
+                        <span class="nomeetodos"> ${listaDados[i].from}</span>
+                        <span class="margin">reservadamente para</span>
+                        <span class="nomeetodos"> ${listaDados[i].to}:</span>
+                        <span class="quebra">${listaDados[i].text}</span>
+                    </div>
+                </li>
+                `;
+        }
     }
-    mostraPrimeiraMensagem()    
+    mostraPrimeiraMensagem()
 }
 
 function mostraPrimeiraMensagem() {
@@ -106,6 +120,53 @@ function mostraPrimeiraMensagem() {
 
 }
 
+function postarMensagem(elemento) {
+    const mensagemEscrita = document.querySelector(".input").value;
+    const mensagemObjeto =
+    {
+        from: nome,
+        to: "Todos",
+        text: mensagemEscrita,
+        type: "message",
+    }
+    //message
+    //private_message
+
+    const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", mensagemObjeto)
+    promise.then(envioConfirmado)
+    promise.catch(naoEnviado)
+   notificacaoEnvio()
+}
+
+function notificacaoEnvio(){
+    const notf = document.querySelector(".notificacaoenvio")
+    notf.classList.remove("escondido")
+}
+
+function tempoNotificacao(){
+    const notf = document.querySelector(".notificacaoenvio")
+    notf.classList.add("escondido")
+}
+
+function envioConfirmado(resposta) {
+    console.log("foi enviado")
+    const input = document.querySelector(".input").value = ""
+    e = 0;
+    buscarMensagens()
+    tempoNotificacao()
+}
+
+function naoEnviado(resposta) {
+    alert()
+    console.log("Não enviada")
+    window.location.reload()
+}
+
+function telaescolha(elemento){
+const conteudo = document.querySelector(".telaestado2")
+conteudo.classList.remove("escondido")
+}
+
 function conexaoSistema() {
     novoNome =
     {
@@ -114,44 +175,16 @@ function conexaoSistema() {
     const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/status", novoNome)
     promise.then(estaConectado)
     promise.catch(desconectado)
-    setTimeout(conexaoSistema, 5000)
 
+    setTimeout(conexaoSistema, 5000)
 }
+
+
 function estaConectado() {
     console.log("ainda conectado")
 }
 function desconectado() {
-    console.log("ainda conectado")
-}
-
-function postarMensagem(elemento) {
-    const mensagemEscrita = document.querySelector(".input").value;
-    const mensagemObjeto =
-    {
-        from: nome,
-        to: "cirilo",
-        text: mensagemEscrita,
-        type: "message",   
-    }
-    //message
-    //private_message
-    
-    const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", mensagemObjeto)
-    promise.then(envioConfirmado)
-    promise.catch(naoEnviado)
-}
-
-function envioConfirmado(resposta) {
-    const input = document.querySelector(".input").value = ""
-    buscarMensagens()
-}
-
-function naoEnviado(resposta) {
-    console.log("Não enviada")
-    window.location.reload()
-}
-
-function telaescolha(elemento){
-const conteudo = document.querySelector(".telaestado2")
-conteudo.classList.remove("escondido")
+    console.log("desconectado")
+    alert("Você foi desconectado")
+    perguntaNome()
 }
